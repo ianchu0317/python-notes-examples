be94029541f85d8613e1b3211d82cc05ee76d84b
Ian Chen <71509578+ianchu0317@users.noreply.github.com>
2022-11-01 11:18:20 -0300

    map() with output

--- functions/map func.py
+++ functions/map func.py
@@ -5,3 +5,9 @@ print("array is: ", array)
 
 array = map(lambda x: x+1, array)
 print("modified array is: ", list(array))
+
+
+"""
+array is:  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+modified array is:  [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+"""
